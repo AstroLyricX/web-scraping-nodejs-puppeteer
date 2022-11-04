1cca9c7829b9c525c0bb576ddba43706f3ea89dd
Ever Ronaldo <ever.condori@upeu.edu.pe>
2022-11-04 04:53:44 -0500

    Se agrego el archivo data-cusco.j s

---
+++ data/data-cusco.js
@@ -0,0 +1,182 @@
+const hostelsCusco = [
+    {
+      title: 'Cusco Plaza Nazarenas',
+      adress: 'Plaza Nazarenas N°181 , Centro de Cusco, 08* Cuzco, Perú ',
+      fifeedback: '8,1',
+      numberComments: '60 comentarios'
+    },
+    {
+      title: 'Hotel Monasterio San Pedro',
+      adress: 'Calle Ccascaparo 116 - Monasterio San Pedro, Centro de Cusco, Cusc01 Cuzco, Perú ',
+      fifeedback: '8,5',
+      numberComments: '961 comentarios'
+    },
+    {
+      title: 'Casona Plaza Hotel Cusco',
+      adress: '490 Tecsecocha, Centro de Cusco, Cuzco, Perú ',
+      fifeedback: '8,5',
+      numberComments: '139 comentarios'
+    },
+    {
+      title: 'El Mariscal Cusco',
+      adress: 'Urb. Lucrepata F-6 - San Blas, Centro de Cusco, Cuz 01 Cuzco, Perú ',
+      fifeedback: '8,7',
+      numberComments: '898 comentarios'
+    },
+    {
+      title: 'Illas Inn',
+      adress: 'Av. Huascar 224-B Wanchaq, Centro de Cusco, Cuzco, Perú ',
+      fifeedback: '9,0',
+      numberComments: '50 comentarios'
+    },
+    {
+      title: 'Mistico San Blas Boutique Hotel',
+      adress: 'Siete Angelitos N° 640, Centro de Cusco, Cuzco, Perú ',
+      fifeedback: '8,8',
+      numberComments: '302 comentarios'
+    },
+    {
+      title: 'Torre Dorada Cusco',
+      adress: 'Calle Los Cipreses, 5 - Residencial Huancaro, Cusc 01 Cuzco, Perú ',
+      fifeedback: '9,2',
+      numberComments: '264 comentarios'
+    },
+    {
+      title: 'Hotel Las Quenas',
+      adress: 'Calle Los Angeles, 1265, Cuzco, Perú ',
+      fifeedback: '8,8',
+      numberComments: '26 comentarios'
+    },
+    {
+      title: 'Costa del Sol Wyndham Cusco',
+      adress: 'Calle Santa Teresa, 344, Centro de Cusco, Cusco Cuzco, Perú ',
+      fifeedback: '8,8',
+      numberComments: '1.844 comentarios'
+    },
+    {
+      title: 'Amaru Colonial',
+      adress: 'Calle Chihuampata, 642, Centro de Cusco, 84 Cuzco, Perú ',
+      fifeedback: '9,0',
+      numberComments: '490 comentarios'
+    },
+    {
+      title: 'Apartamentos Quewe',
+      adress: 'Urbanización Lucrepata F-21 departamento #303, Centro de Cusco, Cuzco, Perú ',
+      fifeedback: '9,4',
+      numberComments: '203 comentarios'
+    },
+    {
+      title: 'Hotel Casa Fierro Cusco',
+      adress: 'Calle Fierro 495, Centro de Cusco, Cuzco, Perú ',
+      fifeedback: '8,4',
+      numberComments: '138 comentarios'
+    },
+    {
+      title: 'Hotel Ruinas',
+      adress: 'Calle Ruinas, 472, Centro de Cusco, Cuzco, Perú ',
+      fifeedback: '8,5',
+      numberComments: '66 comentarios'
+    },
+    {
+      title: 'Hotel Agustos Cusco',
+      adress: 'Avenida Hipólito Unánue N°155, Cuzco, Perú ',
+      fifeedback: '8,3',
+      numberComments: '15 comentarios'
+    },
+    {
+      title: 'Hilton Garden Inn Cusco',
+      adress: 'Santa Ana - Av. Abancay (Ex Av. Apurimac) Nro 207, Centro de Cusco, Cuzco, Perú ',
+      fifeedback: '8,7',
+      numberComments: '676 comentarios'
+    },
+    {
+      title: 'Royal Inka II',
+      adress: 'Santa Teresa, 335, Centro de Cusco, 084 Cuzco, Perú ',
+      fifeedback: '8,5',
+      numberComments: '33 comentarios'
+    },
+    {
+      title: 'El Truco Hotel Boutique',
+      adress: 'Plaza del Regocijo 261, Centro de Cusco, Cuzco, Perú ',
+      fifeedback: '7,8',
+      numberComments: '133 comentarios'
+    },
+    {
+      title: 'Abittare Hotel',
+      adress: 'Plazoleta Santo Domingo 263, Centro de Cusco, 084 Cuzco, Perú ',
+      fifeedback: '8,8',
+      numberComments: '113 comentarios'
+    },
+    {
+      title: 'Hostal Qolqampata',
+      adress: 'Avenida Don Bosco A-7, Centro de Cusco, Cusc 01 Cuzco, Perú ',
+      fifeedback: '8,6',
+      numberComments: '460 comentarios'
+    },
+    {
+      title: 'Departamentos Santa Ana Barrio Tradicional',
+      adress: 'Jiron Los Libertadores 38, Barrio de Santa Ana, Centro de Cusco, Cuzco, Perú ',
+      fifeedback: '8,8',
+      numberComments: '125 comentarios'
+    },
+    {
+      title: 'San Agustin El Dorado',
+      adress: 'Av. El Sol 395, Centro de Cusco, 084 Cuzco, Perú ',
+      fifeedback: '8,3',
+      numberComments: '295 comentarios'
+    },
+    {
+      title: 'Rumi Wasi',
+      adress: 'Calle Suecia Nº 499 , Centro de Cusco, Cusc 01 Cuzco, Perú ',
+      fifeedback: '9,2',
+      numberComments: '34 comentarios'
+    },
+    {
+      title: 'Antigua Casona San Blas',
+      adress: 'Calle Carmen Bajo 243 San Blas, Centro de Cusco, Cuzco, Perú ',
+      fifeedback: '9,7',
+      numberComments: '937 comentarios'
+    },
+    {
+      title: 'ANAHUARQUE Hotel Boutique',
+      adress: 'San Agustin 236, Centro de Cusco, Cusc 01 Cuzco, Perú ',
+      fifeedback: '8,3',
+      numberComments: '370 comentarios'
+    },
+    {
+      title: 'Xima Exclusive Cusco',
+      adress: 'Av. El Sol 1010, Centro de Cusco, 08200 Cuzco, Perú ',
+      fifeedback: '8,7',
+      numberComments: '370 comentarios'
+    },
+    {
+      title: 'Hotel Casa Campesina',
+      adress: 'Avenida Tullumayo, 274, Centro de Cusco, Cusco Cuzco, Perú ',
+      fifeedback: '8,1',
+      numberComments: '220 comentarios'
+    },
+    {
+      title: 'Rituales Inn Cusco - Nazarenas',
+      adress: 'Calle Pumacurco 428, Centro de Cusco, Cuzco, Perú ',
+      fifeedback: '7,0',
+      numberComments: '2 comentarios'
+    },
+    {
+      title: 'AMTALLPA san blas inn',
+      adress: 'Cuesta San Blas 526, Centro de Cusco, Cusc01 Cuzco, Perú ',
+      fifeedback: '7,8',
+      numberComments: '37 comentarios'
+    },
+    {
+      title: 'Hotel Daylu',
+      adress: 'Avenida Tupac Amaru A11, Urbanizacion Progreso Wanchaq, Cuzco, Perú ',
+      fifeedback: '8,1',
+      numberComments: '69 comentarios'
+    },
+    {
+      title: 'Hostal Danna Inn',
+      adress: 'Av. El Sol 809, Centro de Cusco, 084 Cuzco, Perú ',
+      fifeedback: '8,0',
+      numberComments: '56 comentarios'
+    },
+  ]
