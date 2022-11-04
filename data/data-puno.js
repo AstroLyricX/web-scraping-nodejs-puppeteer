8f72968c6650179a07d25ea7f51eb62d076eadff
Ever Ronaldo <ever.condori@upeu.edu.pe>
2022-11-04 04:57:45 -0500

    Se agrego el archivo data-puno.js

---
+++ data/data-puno.js
@@ -0,0 +1,152 @@
+const hostelsPuno = [
+    {
+      title: 'Casona Plaza Hotel Puno',
+      adress: 'Jiron Puno Nº 280 - Cercado, 051 Puno, Perú ',
+      fifeedback: '8,8',
+      numberComments: '509 comentarios'
+    },
+    {
+      title: 'Qelqatani Hotel',
+      adress: 'Jr. Tarapacá, 355, Puno Puno, Perú ',
+      fifeedback: '8,4',
+      numberComments: '165 comentarios'
+    },
+    {
+      title: 'Hotel Hacienda Puno Centro Histórico',
+      adress: 'Jr. Deustua, 297, 051 Puno, Perú ',
+      fifeedback: '8,5',
+      numberComments: '885 comentarios'
+    },
+    {
+      title: 'GHL Hotel Lago Titicaca',
+      adress: 'Isla Esteves s/n - Lago Titicaca, 51 Puno, Perú ',
+      fifeedback: '8,8',
+      numberComments: '503 comentarios'
+    },
+    {
+      title: 'Hotel Hacienda Plaza de Armas',
+      adress: 'Jr. Puno 419, 51 Puno, Perú ',
+      fifeedback: '8,5',
+      numberComments: '648 comentarios'
+    },
+    {
+      title: 'Olimpo Inn',
+      adress: 'Jiron Pedro Miguel Urbina No 155 Ovalo Dante Nava Laykakota, Puno, Perú ',
+      fifeedback: '8,9',
+      numberComments: '460 comentarios'
+    },
+    {
+      title: 'Sonesta Posadas del Inca Puno',
+      adress: 'Av. Sesquicentenario, 610 - Sector Huaje, Puno, Perú ',
+      fifeedback: '8,7',
+      numberComments: '364 comentarios'
+    },
+    {
+      title: 'UROS TITICACA LODGE Puno Peru',
+      adress: 'Uros Titicaca Lodge Floating island, Puno, Perú ',
+      fifeedback: '9,5',
+      numberComments: '227 comentarios'
+    },
+    {
+      title: 'Sol Plaza Hotel',
+      adress: 'Jr. Puno 307, 51 Puno, Perú ',
+      fifeedback: '8,5',
+      numberComments: '694 comentarios'
+    },
+    {
+      title: 'Uros Qhota Uta Lodge',
+      adress: 'Lago Titicaca - Islas de los Uros Islas de los Uros, Puno, Perú ',
+      fifeedback: '8,7',
+      numberComments: '81 comentarios'
+    },
+    {
+      title: 'Xima Exclusive Puno',
+      adress: 'Av. Chulluni 195, 159 Puno, Perú ',
+      fifeedback: '8,6',
+      numberComments: '217 comentarios'
+    },
+    {
+      title: 'Tierra Viva Puno Plaza',
+      adress: 'Calle Grau 270, Puno 01 Puno, Perú ',
+      fifeedback: '9,2',
+      numberComments: '328 comentarios'
+    },
+    {
+      title: 'Hotel Terra Mistica Ciudad del Lago',
+      adress: 'Jr. Conde de Lemus 120 (Esquina con Jr. Libertad), Puno Puno, Perú ',
+      fifeedback: '8,3',
+      numberComments: '263 comentarios'
+    },
+    {
+      title: 'Uros Caminos del Titicaca Peru',
+      adress: 'isla uros zona sur, Puno, Perú ',
+      fifeedback: '10',
+      numberComments: '73 comentarios'
+    },
+    {
+      title: 'Uros Aruma Uro on Uros Floating Islands',
+      adress: 'Islas Flotantes de los Uros, Puno, Perú ',
+      fifeedback: '9,5',
+      numberComments: '123 comentarios'
+    },
+    {
+      title: 'UROs ARUNTAWI LODGE',
+      adress: 'Islas Flotantes de los Uros, Puno, Perú ',
+      fifeedback: '9,3',
+      numberComments: '230 comentarios'
+    },
+    {
+      title: 'Amalia Titicaca Lodge',
+      adress: 'uros Uros Tupiri Corazon, Puno, Perú ',
+      fifeedback: '9,8',
+      numberComments: '121 comentarios'
+    },
+    {
+      title: 'Uros Samaraña Uta Lodge',
+      adress: 'Islas Flotantes de los Uros - Puno - Peru, Puno, Perú ',
+      fifeedback: '9,6',
+      numberComments: '138 comentarios'
+    },
+    {
+      title: 'Uros Lake Titicaca Lodge',
+      adress: 'Islas flotantes Los Uros Titicaca, Puno, Perú ',
+      fifeedback: '9,2',
+      numberComments: '68 comentarios'
+    },
+    {
+      title: 'Casa Don Jose B & B',
+      adress: 'Jr. A. Velasco Astete 132-134, Puno, Perú ',
+      fifeedback: '8,4',
+      numberComments: '59 comentarios'
+    },
+    {
+      title: 'Colonial Plaza Hotel',
+      adress: 'Jr. Puno Nº 529, Puno, Perú ',
+      fifeedback: '8,5',
+      numberComments: '258 comentarios'
+    },
+    {
+      title: 'Casa Andina Standard Puno',
+      adress: 'Jr. Independencia 143, puno 001 Puno, Perú ',
+      fifeedback: '8,9',
+      numberComments: '128 comentarios'
+    },
+    {
+      title: 'Taypikala Lago',
+      adress: 'Panamericana Sur Km 18, Chucuito, Puno 51 Puno, Perú ',
+      fifeedback: '8,5',
+      numberComments: '166 comentarios'
+    },
+    {
+      title: 'Uros Qhantany Lodge',
+      adress: 'Isla Uros Qhantany, Puno, Perú ',
+      fifeedback: '9,4',
+      numberComments: '108 comentarios'
+    },
+    {
+      title: "Marlon's House Puno-Peru",
+      adress: 'Jr. Independencia 255, Puno, Perú ',
+      fifeedback: '8,0',
+      numberComments: '143 comentarios'
+    }
+  ]
